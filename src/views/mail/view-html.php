3e2731dd48b2715f825765d7362f911ff541a24b
wangrunxin <wangrunxinyes@gmail.com>
2018-01-15 16:55:52 +0800

    -update;

---
+++ src/views/mail/view-html.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var View $this */
+/** @var AuditMail $model */
+
+use bedezign\yii2\audit\models\AuditMail;
+use PhpMimeMailParser\Parser;
+use yii\web\View;
+
+$this->title = Yii::t('audit', 'Mail #{id}', ['id' => $model->id]);
+$this->params['breadcrumbs'][] = ['label' => Yii::t('audit', 'Audit'), 'url' => ['default/index']];
+$this->params['breadcrumbs'][] = ['label' => Yii::t('audit', 'Mails'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = '#' . $model->id;
+
+
+//echo Yii::$app->formatter->asHtml($model->html);
+
+$parser = new Parser();
+$parser->setText($model->data);
+echo $parser->getMessageBody('htmlEmbedded');
